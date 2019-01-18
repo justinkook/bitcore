18f5fdba5439cd20b82ea204077b90461cbdaba7
Justin <justin@Satoshis-MacBook-Pro.local>
2019-01-18 09:41:36 -0500

    fixed bug with utxos stream route on bitcore client on bin/wallet functions

--- packages/bitcore-node/test/integration/websocket.integration.ts
+++ packages/bitcore-node/test/integration/websocket.integration.ts
@@ -1,13 +1,28 @@
 import io = require('socket.io-client');
+
 console.log('Attempting socket connection');
 const socket = io.connect('http://localhost:3000', {transports: ['websocket']});
 socket.on('connect', () => {
   console.log('Connected to socket');
   socket.emit('room', '/BTC/regtest/inv');
 });
+
+socket.on('tx', sanitizedTx => {
+  console.log(sanitizedTx);
+});
+
+socket.on('2N17ayiQb5nU7da1NgdG4B4jHE3k4B5UKaC', sanitizedCoin => {
+  console.log(sanitizedCoin);
+});
+
+socket.on('coin', sanitizedCoin => {
+  console.log(sanitizedCoin);
+});
+
 socket.on('block', payload => {
   console.log(payload);
 });
+
 socket.on('disconnect', () => {
   console.log('Socket disconnected');
 });
