b866b19bacd9202e58f57ba79a8117aa6e188a9d
Justin Kook <jkook@bitpay.com>
2019-06-27 18:37:29 -0400

    revert to master

--- packages/bitcore-client/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Wallet } from './wallet';
-import { Storage } from './storage';
-import { Client } from './client';
-import { ParseApiStream } from './stream-util';
-export { Wallet, Client, Storage, ParseApiStream };
